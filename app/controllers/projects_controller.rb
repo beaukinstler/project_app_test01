0155e9f6a11a3d771f0c932ec0a352bf8c02c920
Beau Kinstler <git@beaukinstler.info>
2016-05-10 00:15:27 -0500

    success realating Project and User classes allowing a user to either or both an owner or sponsor

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,6 +69,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :owner_id)
+      params.require(:project).permit(:name, :owner_id, :sponsor_id)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,5 @@
 class User < ActiveRecord::Base
-
+  has_many :owned_projects, class_name: "Project", foreign_key: :owner_id
+  has_many :sponsored_projects, class_name: "Project", foreign_key: :sponsor_id
 end
 
-
-class Owner < User
-  has_many :projects, :foreign_key => :owner_id
-end
-
-
-class Sponsor < User
-  has_many :projects, :foreign_key => :sponsor_id
-end
